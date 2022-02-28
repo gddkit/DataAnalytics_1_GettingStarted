352267f8ff36720f5f2d2df1375a75a75a773f5e
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-02-28 10:32:27 +0000

    Intro to types and operators

--- 1_GettingStarted.R
+++ 1_GettingStarted.R
@@ -1,1 +1,52 @@
-# Data Analytics - Getting Started - Niall McGuinness
+# Data044L Analytics - Getting Started - Niall McGuinness
+
+#assignment operator
+s <- 10
+y <- "jane"
+z <- TRUE
+
+# assigning 100 to 3 variables i.e. l, m, n
+l <- m <- n <- 100
+
+
+# data types (numeric, integer, complex, character, logical)
+height <- 1.9
+class(height)
+
+#integer
+population <- 1044L
+
+#complex
+x <- 3i + 4
+
+#character
+name <- "jane smith"
+fname <- 'Jane'
+
+#logical (capitalised) 
+isRaining <- TRUE 
+
+# operators (arithmetic, logical)
+age1 <- 24
+age2 <- 35
+average <- age1 + age2
+
+# <
+# <=
+if(age1 <= age2)  #less than or equal
+{
+  print("You are younger then me")
+}
+
+
+# control flow (e.g. if)
+
+# loops (while, for, repeat/break)
+
+# function
+
+# input
+
+# read
+
+#statistical
