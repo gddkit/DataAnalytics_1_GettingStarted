60c58bae1a164fcffd8f9fbfcc021d8f3d92e9d4
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-03-02 09:26:55 +0000

    Add operator, loop, and function demos

--- 1_GettingStarted.R
+++ 1_GettingStarted.R
@@ -1,13 +1,20 @@
-# Data044L Analytics - Getting Started - Niall McGuinness
+# Author:       Niall McGuinness
+# Title:        Getting Started
+# Description:  Illustrating types, operators, constants, and functions 
+# Date:         03/2022
+# Version:      1.0
+# Note:         Variable names are chosen for simplicity and would not reflect the best choice of varaible name within a typical script
+# See:          https://iqss.github.io/dss-workshops/R/Rintro/base-r-cheat-sheet.pdf
+#               https://www.guru99.com/r-functions-programming.html
+#               https://www.delftstack.com/howto/r/value-of-e-in-r/
+#               https://www.geeksforgeeks.org/taking-input-from-user-in-r-programming/
+#               https://www.earthdatascience.org/courses/earth-analytics/multispectral-remote-sensing-data/source-function-in-R/
 
-#assignment operator
-s <- 10
-y <- "jane"
-z <- TRUE
+# cat is the concatenate and print function 
+# The literal "\014" sends a CTRL + L to the console to clear it
+cat("\014") 
 
-# assigning 100 to 3 variables i.e. l, m, n
-l <- m <- n <- 100
-
+############################ DATA TYPES ############################
 
 # data types (numeric, integer, complex, character, logical)
 height <- 1.9
@@ -26,14 +33,30 @@ fname <- 'Jane'
 #logical (generally ALWAYS capitalised) 
 isRaining <- TRUE 
 
+############################ OPERATORS ############################
+
+#assignment operator
+s <- 10
+y <- "jane"
+z <- TRUE
+
+# assigning 100 to 3 variables i.e. l, m, n
+l <- m <- n <- 100
+
 # operators (arithmetic, logical)
 age1 <- 24
 age2 <- 35
-average <- age1 + age2
+sum <- age1 + age2
+sum
+modResult <- 10 %% 3
+modResult
+
 
 # logical operator
 isYounger <- age1 <= age2 #isYounger will be TRUE or FALSE
-paste("Value of isYounger is", isYounger)
+cat("Value of isYounger is", isYounger)
+
+############################ CONTROL FLOW ############################
 
 # control flow (e.g. if)
 if(age1 <= age2)  #less than or equal
@@ -41,12 +64,80 @@ if(age1 <= age2)  #less than or equal
   print("You are younger then I!")
 }
 
-# loops (while, for, repeat/break)
+# control flow (e.g. if...else)
+today <- "sat"
+if(today == "fri"){
+  print("It's the start of the weekend!")
+}else{
+  print("Still a few days until the start of the weekend!")
+}
 
-# function
+############################ LOOPS ############################
+
+# loops (while, for, repeat/break(we will not use this loop construct))
+count <- 1
+while(count < 5)
+{
+  print("Count is...")
+  print(count)
+  count <- count + 1
+}
+
+for(i in -5:5)
+{
+  print(i)
+}
+
+
+############################ MATH FUNCTIONS & CONSTANTS ############################
+# in-built functions
+logTest1 = log10(64); logTest1
+logTest2 = log2(64); logTest2
+logTest3 = log(64, exp(1)); logTest3
+
+expTest1 = exp(4); expTest1
+powTest1 = 2^4; powTest1
+sqrtTest1 = sqrt(25); sqrtTest1
+
+absTest1 = abs(-10.58); absTest1
+absTest2 = abs(-1E6); absTest2
+
+# in-built constants
+radius = 1
+area1 = 2 * pi * radius * radius
+paste("Circle area is ", area1)
+
+############################ USER-DEFINED FUNCTIONS ############################
+
+# user-defined functions
+
+square <- function(x){
+  x * x
+}
+
+theSquare = square(12); theSquare
+
+diff <- function(a,b){
+  a - b
+}
+
+theDiff = diff(4, 10); theDiff
+
+############################ USER-INPUT ############################
 
 # input
+var = readline(prompt = "Enter a number:");
 
-# read
+# convert the inputted value to an integer
+var = as.integer(var);
 
-#statistical
+# print the value
+print(var)
+
+############################ FILE-INPUT ############################
+
+
+############################ DATA STRUCTURES ############################
+
+
+
---
+++ 2_StatisticalAndPlotFunctions.R
@@ -0,0 +1,22 @@
+# Author:       Niall McGuinness
+# Title:        Core Statistical & Plot Functions
+# Description:  Illustrating basic statistical and plot functions 
+# Date:         03/2022
+# Version:      1.0
+# See:          https://iqss.github.io/dss-workshops/R/Rintro/base-r-cheat-sheet.pdf
+#               https://www.guru99.com/r-functions-programming.html
+#               https://www.dummies.com/article/technology/programming-web-design/r/statistical-analysis-with-r-for-dummies-cheat-sheet-262959
+#               https://cran.r-project.org/web/packages/bannerCommenter/vignettes/Banded_comment_maker.pdf
+
+############################ STATISTICAL FUNCTIONS ############################
+
+#core statistical functions
+#mean, sd, var, median, summary, pnorm
+
+
+############################ PLOT FUNCTIONS ############################
+
+#plot
+
+v = rep(1:2, times=3); v
+z = sort(v); z
