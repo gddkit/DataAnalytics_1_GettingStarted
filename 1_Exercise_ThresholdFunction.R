b91c966527298f5b1f73740c2be95578ebb9879f
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-03-07 09:32:40 +0000

    Added itHistStretch solution

--- 1_Exercise_ThresholdFunction.R
+++ 1_Exercise_ThresholdFunction.R
@@ -2,3 +2,13 @@
 #           where
 #           max: largest, min: smallest, current: value represent the variables to be stretched
 # See https://www.tutorialspoint.com/dip/histogram_stretching.htm
+
+# mapping from (min, max) to (0, max)
+# function(parameters) and when we invoke we do itHistStretch(10, 20, 15) and 10, 20, 15 are arguments
+itHistStretch <- function(min, max, current){
+  #no return keyword
+  max * ((current - min)/(max - min))
+}
+
+# invoke with specific arguments
+stretchedResult < - itHistStretch(10, 20, 15)
