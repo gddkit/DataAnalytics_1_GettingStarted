f1c3cf5282d10145cd67644bdf6e10580b0a7d75
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-04-27 10:09:56 +0100

    Corrected chamomile spelling

--- Examples/Exercises/6_Exercise_Chamomile_Inference.R
+++ Examples/Exercises/6_Exercise_Chamomile_Inference.R
@@ -1,13 +1,13 @@
 y <- dnorm(100, 4.7, 0.65)
 hist(y, 
-     main = "Women - Frequency of Normally Distributed Random Values", 
+     main = "Women - Distribution of perceived calming effect of chamomile tea", 
      ylab = "Frequency",
      col = "yellow")
      
 
 z <- dnorm(100, 3.2, 0.85)
 hist(z, 
-     main = "Men - Frequency of Normally Distributed Random Values", 
+     main = "Men - Distribution of perceived calming effect of chamomile tea", 
      ylab = "Frequency",
      col = "red")
 
